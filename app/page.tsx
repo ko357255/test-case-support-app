85674edb32a4c761419429a3da0e3c960ce3dfd8
ko357255 <mikanace@outlook.jp>
2025-12-04 12:26:37 +0900

    format

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import { Button } from '@/components/ui/button';
 
 export default function Home() {
   return (
-    <main className="flex items-center justify-center h-screen">
+    <main className="flex h-screen items-center justify-center">
       <h1 className="text-3xl font-bold">Hello World</h1>
       <Button>これはshadcn/ui のボタンです</Button>
     </main>
--- prettier.config.js
+++ prettier.config.js
@@ -2,9 +2,9 @@
 module.exports = {
   semi: true,
   singleQuote: true,
-  trailingComma: "all",
-  arrowParens: "always",
+  trailingComma: 'all',
+  arrowParens: 'always',
   printWidth: 100,
   tabWidth: 2,
-  plugins: ["prettier-plugin-tailwindcss"],
+  plugins: ['prettier-plugin-tailwindcss'],
 };
