5ad7e400573099c1d216a9bbc508b70bfe0af0d7
ko357255 <mikanace@outlook.jp>
2025-12-11 11:26:31 +0900

    条件欄を追加

--- app/layout.tsx
+++ app/layout.tsx
@@ -33,7 +33,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
       {/* フォントをCSS変数に登録する */}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
--- components/layout/Sidebar.tsx
+++ components/layout/Sidebar.tsx
@@ -1,7 +1,31 @@
 export default function Sidebar() {
   return (
-    <aside className="w-64 border-r border-gray-200 bg-white">
-      ここはサイドバーです
+    // 縦に並べる
+    <aside className="flex w-64 flex-col border-r border-gray-200 bg-white">
+      サイドバー
+      <div className="border-b border-gray-200 p-6">見出し</div>
+      {/* 縦に並べる 縦方向をスクロール可 */}
+      <div className="flex-1 overflow-y-auto p-6">
+        コンテンツ
+        <div className="mb-6">
+          <div className="mb-2 text-gray-700">条件名</div>
+          <div className="ml-3">
+            <div className="py-1 text-gray-700">条件</div>
+            <div className="py-1 text-gray-700">条件</div>
+            <div className="py-1 text-gray-700">条件</div>
+            <div className="py-1 text-gray-700">条件</div>
+          </div>
+        </div>
+        <div className="mb-6">
+          <div className="mb-2 text-gray-700">条件名</div>
+          <div className="ml-3">
+            <div className="py-1 text-gray-700">条件</div>
+            <div className="py-1 text-gray-700">条件</div>
+            <div className="py-1 text-gray-700">条件</div>
+            <div className="py-1 text-gray-700">条件</div>
+          </div>
+        </div>
+      </div>
     </aside>
   );
 }
