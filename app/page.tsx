7bbd9a2b59d089d269e64555505dc999350e4da7
ko357255 <mikanace@outlook.jp>
2025-12-12 01:08:20 +0900

    更新：テストケース一覧を表示するよう変更

--- app/page.tsx
+++ app/page.tsx
@@ -1,32 +1,15 @@
-export default function Home() {
+import TestCaseTable from '@/components/features/testcases/TestCaseTable';
+import { fetchTestCases } from '@/lib/api/testcases';
+
+/**
+ * ホーム（サーバー）
+ */
+export default async function Home() {
+  // テストケースの一覧を取得
+  const testcases = await fetchTestCases();
   return (
     <div className="px-8 py-4">
-      メインコンテンツ
-      <table className="w-full border-collapse border border-gray-300">
-        <thead className="bg-gray-200">
-          <tr className="border-b border-gray-300 text-left">
-            <th className="px-6 py-2">ステータス</th>
-            <th className="px-6 py-2">タイトル</th>
-            <th className="px-6 py-2">カテゴリ</th>
-            <th className="px-6 py-2">優先度</th>
-          </tr>
-        </thead>
-
-        <tbody className="divide-y divide-gray-300">
-          <tr>
-            <td className="px-6 py-3">未完了</td>
-            <td className="px-6 py-3">HTMLの描画</td>
-            <td className="px-6 py-3">HTML</td>
-            <td className="px-6 py-3">高</td>
-          </tr>
-          <tr>
-            <td className="px-6 py-3">成功</td>
-            <td className="px-6 py-3">tailwindの反映</td>
-            <td className="px-6 py-3">CSS</td>
-            <td className="px-6 py-3">高</td>
-          </tr>
-        </tbody>
-      </table>
+      <TestCaseTable testCases={testcases} />
     </div>
   );
 }
