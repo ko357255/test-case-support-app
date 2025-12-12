a29add21f1ff2e1d1af3cfb41f975cd5b7d1f4ca
ko357255 <mikanace@outlook.jp>
2025-12-12 09:34:10 +0900

    変更：ベースURLを環境変数に変更

--- lib/api/testcases.ts
+++ lib/api/testcases.ts
@@ -1,10 +1,12 @@
 import { TestCase } from '@/types/testcase';
 
+const API_BASE_URL: string = process.env.NEXT_PUBLIC_API_BASE_URL!;
+
 /**
  * テストケース一覧を取得するAPI関数
  */
 export async function fetchTestCases(): Promise<TestCase[]> {
-  const response = await fetch('http://192.168.2.105:3000/api/testcases', {
+  const response = await fetch(`${API_BASE_URL}/api/testcases`, {
     cache: 'no-store', // 常に最新を取得
   });
   if (!response.ok) {
@@ -18,7 +20,7 @@ export async function fetchTestCases(): Promise<TestCase[]> {
  * @param id テストケースID
  */
 export async function fetchTestCaseById(id: string): Promise<TestCase> {
-  const res = await fetch(`http://192.168.2.105:3000/api/testcases/${id}`, {
+  const res = await fetch(`${API_BASE_URL}/api/testcases/${id}`, {
     cache: 'no-store',
   });
 
