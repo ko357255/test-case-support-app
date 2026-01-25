848cc9179238f28b954f2e435f9ba9d8f7eafb08
ko357255 <mikanace@outlook.jp>
2026-01-25 17:18:09 +0900

    追加：認証情報のプロバイダーを追加

--- components/Providers.tsx
+++ components/Providers.tsx
@@ -1,16 +1,20 @@
 'use client';
 
 import { ThemeProvider } from 'next-themes';
+import { AuthProvider } from './auth/AuthProvider';
 
 export default function Providers({ children }: { children: React.ReactNode }) {
   return (
-    // テーマの設定
-    <ThemeProvider
-      attribute="class" // clas="dark" を付ける
-      defaultTheme="system" // 初期値はシステム設定に合わせる
-      enableSystem // システム設定の変更を監視する
-    >
-      {children}
-    </ThemeProvider>
+    // 認証の設定
+    <AuthProvider>
+      {/* テーマの設定 */}
+      <ThemeProvider
+        attribute="class" // clas="dark" を付ける
+        defaultTheme="system" // 初期値はシステム設定に合わせる
+        enableSystem // システム設定の変更を監視する
+      >
+        {children}
+      </ThemeProvider>
+    </AuthProvider>
   );
 }
