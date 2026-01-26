d1136bfdedbcdc482f6fe512c1b4c772de2e10b0
ko357255 <mikanace@outlook.jp>
2026-01-26 21:13:53 +0900

    ドキュメント：コメントを追加

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,18 +2,27 @@ import type { NextAuthOptions } from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { adminAuth } from '@/lib/firebase-admin';
 
+/**
+ * NextAuthの設定
+ */
 export const authOptions: NextAuthOptions = {
+  // ログイン方法の登録
   providers: [
+    // メールアドレスによるログイン
     CredentialsProvider({
       name: 'Firebase Email Login',
+      // 受け取るデータ
       credentials: {
+        // ログイン時に Firebase から idToken を受け取る
         idToken: { type: 'text' },
       },
+      // 認証関数
       async authorize(credentials) {
         if (!credentials?.idToken) return null;
-
+        // idToken をfirebaseで検証する
         const decoded = await adminAuth.verifyIdToken(credentials.idToken);
 
+        // ログイン成功時にデータを返す
         return {
           id: decoded.uid,
           uid: decoded.uid,
@@ -22,13 +31,16 @@ export const authOptions: NextAuthOptions = {
       },
     }),
   ],
+  // JWTの秘密鍵
   secret: process.env.NEXTAUTH_SECRET,
 
   pages: {
-    signIn: '/login',
+    signIn: '/login', // 未ログイン時に遷移する
   },
 
+  // セッションのカスタマイズ
   callbacks: {
+    // jwt に uid を含めるよう変更
     async jwt({ token, user }) {
       if (user) {
         token.uid = user.id;
@@ -36,6 +48,7 @@ export const authOptions: NextAuthOptions = {
       return token;
     },
 
+    // セッションユーザーに uid を含めるよう変更
     async session({ session, token }) {
       session.user.uid = token.uid as string;
       return session;
--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -2,16 +2,13 @@ import { cert, getApps, initializeApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
 
 if (getApps().length === 0) {
+  // Firebase Admin SDK の初期化
   initializeApp({
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
     }),
-    // credential: cert(
-    //   // 環境変数から認証情報を取得
-    //   JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT_KEY as string),
-    // ),
   });
 }
 
