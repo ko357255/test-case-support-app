7c57d6b7a455229a6ac34358c1e4a58baf3c1c30
ko357255 <mikanace@outlook.jp>
2026-01-29 09:57:04 +0900

    修正：軽微

--- components/features/testcases/detail/TestCaseEvidenceList.tsx
+++ components/features/testcases/detail/TestCaseEvidenceList.tsx
@@ -82,7 +82,7 @@ export default function TestCaseEvidenceList({
                               )
                             }
                             placeholder="テキスト内容"
-                            className="border-input bg-background text-foreground ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring min-h-[80px] w-full rounded-md border px-2 py-1 text-xs focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none"
+                            className="border-input bg-background text-foreground ring-offset-background placeholder:text-muted-foreground focus-visible:ring-ring min-h-20 w-full rounded-md border px-2 py-1 text-xs focus-visible:ring-2 focus-visible:ring-offset-2 focus-visible:outline-none"
                           />
                         ) : evidence.url ? (
                           <div className="text-xs">
