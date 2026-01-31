751f0884272a4067839821dd915df64243e319ff
ko357255 <mikanace@outlook.jp>
2026-01-31 16:25:03 +0900

    修正：不要なページを削除

--- app/(app)/projects/[projectId]/testcases/create/page.tsx
+++
@@ -1,8 +0,0 @@
-import TestCaseCreate from '@/components/features/testcase/detail/TestCaseCreate';
-
-/**
- * テストケース新規作成ページ（サーバー）
- */
-export default function Page() {
-  return <TestCaseCreate />;
-}
