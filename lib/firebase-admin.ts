68aff940c570560f1b561204a4cfadf700fa94f9
ko357255 <mikanace@outlook.jp>
2026-01-31 23:28:26 +0900

    追加：adminDB

--- lib/firebase-admin.ts
+++ lib/firebase-admin.ts
@@ -1,5 +1,6 @@
 import { cert, getApps, initializeApp } from 'firebase-admin/app';
 import { getAuth } from 'firebase-admin/auth';
+import { getFirestore } from 'firebase-admin/firestore';
 
 if (getApps().length === 0) {
   // Firebase Admin SDK の初期化
@@ -13,3 +14,4 @@ if (getApps().length === 0) {
 }
 
 export const adminAuth = getAuth();
+export const adminDb = getFirestore();
