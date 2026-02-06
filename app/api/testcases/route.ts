c0e3dcd82144feec35d5fee90571ee5bd16737a3
ko357255 <mikanace@outlook.jp>
2026-02-06 13:27:33 +0900

    修正：不要なファイルを削除

--- app/api/testcases/route.ts
+++
@@ -1,9 +0,0 @@
-// import mockTestcases from '@/data/testcases.json';
-
-// /**
-//  * サーバー テストケースの配列を返すAPIエンドポイント（モック）
-//  * GET /api/testcases
-//  */
-// export async function GET() {
-//   return Response.json(mockTestcases);
-// }
